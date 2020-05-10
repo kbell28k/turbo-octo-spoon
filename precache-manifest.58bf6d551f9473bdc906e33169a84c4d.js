a1c8fbe51892fd71d5d75aecb43f1628eb03a628
Mona Lisa <kbell28k@gmail.com>
2020-05-09 21:15:06 -0600

    Updates

---
+++ precache-manifest.58bf6d551f9473bdc906e33169a84c4d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c328fb55c10e3cd2118",
+    "url": "/turbo-octo-spoon/static/css/main.fb22a792.chunk.css"
+  },
+  {
+    "revision": "5c328fb55c10e3cd2118",
+    "url": "/turbo-octo-spoon/static/js/main.a3825363.chunk.js"
+  },
+  {
+    "revision": "87c40d29d668710f1450",
+    "url": "/turbo-octo-spoon/static/js/runtime~main.60b38646.js"
+  },
+  {
+    "revision": "de29261a34e8dff355bf",
+    "url": "/turbo-octo-spoon/static/js/2.0be49749.chunk.js"
+  },
+  {
+    "revision": "6f4fe3ca482221e7c0d8c1baa5f31100",
+    "url": "/turbo-octo-spoon/static/media/CircularStd-Bold.6f4fe3ca.woff"
+  },
+  {
+    "revision": "2153a12d1db4eda5b954e54532175e5d",
+    "url": "/turbo-octo-spoon/static/media/CircularStd-BlackItalic.2153a12d.woff"
+  },
+  {
+    "revision": "b5fa1b553516d1cf8d03d6b4588058a0",
+    "url": "/turbo-octo-spoon/static/media/CircularStd-Black.b5fa1b55.woff"
+  },
+  {
+    "revision": "8d5dbe8149820b1202cbaf34b7204598",
+    "url": "/turbo-octo-spoon/static/media/CircularStd-BoldItalic.8d5dbe81.woff"
+  },
+  {
+    "revision": "c47d7e581f2a563f8dedd4509c106fb8",
+    "url": "/turbo-octo-spoon/static/media/CircularStd-Book.c47d7e58.woff"
+  },
+  {
+    "revision": "e47ea4c0b9151fe6cbd8e2f332113559",
+    "url": "/turbo-octo-spoon/static/media/CircularStd-Medium.e47ea4c0.woff"
+  },
+  {
+    "revision": "eddb70218f38749eaeeed7fba894fda8",
+    "url": "/turbo-octo-spoon/static/media/CircularStd-MediumItalic.eddb7021.woff"
+  },
+  {
+    "revision": "cc06409e5a820f455b3b4883fbf9b72f",
+    "url": "/turbo-octo-spoon/static/media/CircularStd-BookItalic.cc06409e.woff"
+  },
+  {
+    "revision": "be12bc7b5102a0f2ecb0b9253dad0de7",
+    "url": "/turbo-octo-spoon/static/media/pawpals.be12bc7b.png"
+  },
+  {
+    "revision": "d7ed971130a889957ce5f1b9e3a87964",
+    "url": "/turbo-octo-spoon/static/media/vibestreet.d7ed9711.png"
+  },
+  {
+    "revision": "3eeb7cc8c6e5aa56bebb8bb6f045a06d",
+    "url": "/turbo-octo-spoon/static/media/budget.3eeb7cc8.png"
+  },
+  {
+    "revision": "4b730567918807f31a842c0b1ae4b14f",
+    "url": "/turbo-octo-spoon/static/media/neighbuddy.4b730567.png"
+  },
+  {
+    "revision": "9dacc4cfd8c208ae7a0bd927db6bb50e",
+    "url": "/turbo-octo-spoon/static/media/pawpals-background.9dacc4cf.png"
+  },
+  {
+    "revision": "642f8b5e68d0e91de6430b9c59c20599",
+    "url": "/turbo-octo-spoon/static/media/projectonebackground.642f8b5e.png"
+  },
+  {
+    "revision": "d9005895831261447eac0e6252c72534",
+    "url": "/turbo-octo-spoon/static/media/vibestreet-laptop.d9005895.png"
+  },
+  {
+    "revision": "442ea73fce705d85e689a4ba77c4e075",
+    "url": "/turbo-octo-spoon/static/media/budget-logo.442ea73f.png"
+  },
+  {
+    "revision": "2e9623c3a5fcc923c8442f4b364e1ad8",
+    "url": "/turbo-octo-spoon/static/media/neighbuddy-code.2e9623c3.png"
+  },
+  {
+    "revision": "1d5a27da5bc903b50c3fc96748f6392e",
+    "url": "/turbo-octo-spoon/static/media/profile.1d5a27da.png"
+  },
+  {
+    "revision": "e738de8af8f50c5a30c00180ee184af5",
+    "url": "/turbo-octo-spoon/static/media/Resume_Kayla Bell.e738de8a.pdf"
+  },
+  {
+    "revision": "9dd4203f5af96a07b048f63b39e8fe35",
+    "url": "/turbo-octo-spoon/static/media/link-icon.9dd4203f.svg"
+  },
+  {
+    "revision": "2013cddd18c4bbac658b9277473a8003",
+    "url": "/turbo-octo-spoon/index.html"
+  }
+];
